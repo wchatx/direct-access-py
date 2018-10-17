c309e0b5e801e66a437615ee01277267eaf9d184
wchatx <wchatx@gmail.com>
2018-10-17 06:03:59 -0500

    configurable session, logger. bumb version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 from setuptools import find_packages
 from setuptools.command.install import install
 
-VERSION = '0.4.4'
+VERSION = '0.4.5'
 
 REQUIRES = [
     'requests==2.18.4',
