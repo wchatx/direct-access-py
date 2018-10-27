162ebb811a8244493eaca8d71686060b38e23594
wchatx <wchatx@gmail.com>
2018-10-27 05:35:54 -0500

    unpin requests, logging, setup.cfg

--- setup.py
+++ setup.py
@@ -9,20 +9,19 @@ from setuptools.command.install import install
 VERSION = '0.4.5'
 
 REQUIRES = [
-    'requests==2.18.4',
+    'requests',
     'retrying==1.3.3',
 ]
 
 
 class VerifyVersionCommand(install):
-    """Custom command to verify that the git tag matches our version"""
-    description = 'verify that the git tag matches our version'
+    description = 'verify that version matches the tag prior to circleci pushing to pypi'
 
     def run(self):
         tag = os.getenv('CIRCLE_TAG')
 
         if tag != VERSION:
-            info = "Git tag: {0} does not match the version of this app: {1}".format(
+            info = 'Git tag: {0} does not match the version of this app: {1}'.format(
                 tag, VERSION
             )
             sys.exit(info)
@@ -52,6 +51,9 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5'
         'Programming Language :: Python :: 3.6',
     ],
 )
