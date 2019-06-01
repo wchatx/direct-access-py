cede86de98be734fb634369a8a2b41d17aa5c732
wchatx <wchatx@gmail.com>
2019-06-01 07:31:24 -0500

    bump version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 from setuptools import find_packages
 from setuptools.command.install import install
 
-VERSION = '1.1.3'
+VERSION = '1.2.0'
 
 
 class VerifyVersionCommand(install):
